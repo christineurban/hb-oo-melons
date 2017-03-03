b2b2e32d2531b2a9cf6783b65ef1655ae2245fb4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-02 17:36:55 -0800

    Created superclass and cleaned up subclasses

--- melons.py
+++ melons.py
@@ -1,55 +1,87 @@
 """This file should have our order classes in it."""
 
+class AbstractMelonOrder(object):
+    """Parent class for melon orders.
+    """
 
-class DomesticMelonOrder(object):
+    def __init__(self, species, qty, order_type, tax, country_code = None):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+        self.country_code = country_code
+
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
     def __init__(self, species, qty):
         """Initialize melon order attributes"""
 
-        self.species = species
-        self.qty = qty
-        self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
+        super(DomesticMelonOrder, self).__init__(species, 
+            qty, "domestic", 0.08)
 
-    def get_total(self):
-        """Calculate price."""
+        # self.species = species
+        # self.qty = qty
+        # self.shipped = False
+        # self.order_type = "domestic"
+        # self.tax = 0.08
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
+    # def get_total(self):
+    #     """Calculate price."""
 
-    def mark_shipped(self):
-        """Set shipped to true."""
+    #     base_price = 5
+    #     total = (1 + self.tax) * self.qty * base_price
+    #     return total
 
-        self.shipped = True
+    # def mark_shipped(self):
+    #     """Set shipped to true."""
+
+    #     self.shipped = True
 
 
-class InternationalMelonOrder(object):
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
 
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
+        super(InternationalMelonOrder, self).__init__(species, 
+            qty, "international", 0.17, country_code)
+        # self.species = species
+        # self.qty = qty
+        # self.country_code = country_code
+        # self.shipped = False
+        # self.order_type = "international"
+        # self.tax = 0.17
 
-    def get_total(self):
-        """Calculate price."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
+    # def get_total(self):
+    #     """Calculate price."""
 
-    def mark_shipped(self):
-        """Set shipped to true."""
+    #     base_price = 5
+    #     total = (1 + self.tax) * self.qty * base_price
+    #     return total
 
-        self.shipped = True
+    # def mark_shipped(self):
+    #     """Set shipped to true."""
+
+    #     self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
